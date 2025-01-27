c02589d44060c9624480031fa95149253b6f1496
ngms-psh <psh@ngms.dk>
2025-01-27 19:08:22 +0100

    Fixed Get-CIPPAlertAppSecretExpiry

--- Modules/CIPPCore/Public/Alerts/Get-CIPPAlertAppSecretExpiry.ps1
+++ Modules/CIPPCore/Public/Alerts/Get-CIPPAlertAppSecretExpiry.ps1
@@ -24,7 +24,7 @@ function Get-CIPPAlertAppSecretExpiry {
             foreach ($Credential in $App.passwordCredentials) {
                 if ($Credential.endDateTime -lt (Get-Date).AddDays(30) -and $Credential.endDateTime -gt (Get-Date).AddDays(-7)) {
                     Write-Host ("Application '{0}' has secrets expiring on {1}" -f $App.displayName, $Credential.endDateTime)
-                    @{ DisplayName = $App.displayName; Expires = $Credential.endDateTime }
+                    "App: $($App.displayName) - Secret Expires: $($Credential.endDateTime)"
                 }
             }
         }
