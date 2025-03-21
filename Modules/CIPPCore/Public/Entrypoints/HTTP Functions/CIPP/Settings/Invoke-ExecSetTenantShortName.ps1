e9301cd99febcade234a5cb1b46162b9d8afe0ff
ngms-psh <psh@ngms.dk>
2025-03-21 19:31:03 +0100

    Added Invoke-ExecSetTenantShortName api

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Settings/Invoke-ExecSetTenantShortName.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Settings/Invoke-ExecSetTenantShortName.ps1
@@ -12,59 +12,21 @@ Function Invoke-ExecSetTenantShortName {
 
     Write-LogMessage -headers $Request.Headers -API $APINAME -message 'Accessed this API' -Sev 'Debug'
 
-    $username = $Request.Headers.'x-ms-client-principal-name'
-    $date = (Get-Date).tostring('yyyy-MM-dd')
+    try {
+        $TenantsTable = Get-CippTable -tablename Tenants
+        $Tenant = Get-Tenants -TenantFilter $Request.body.value
+        $Tenant | Add-Member -MemberType NoteProperty -Name shortName -Value $Request.body.ShortName
 
-    Write-host "ExecSetTenantShortName_Request: $($Request | ConvertTo-Json -Depth 100)"
-    Write-LogMessage -API $APINAME -tenant "Test" -headers $Request.Headers -message "$($Request| ConvertTo-Json -Depth 50)" -Sev 'Info'
+        Update-AzDataTableEntity -Force @TenantsTable -Entity $Tenant
+        Write-LogMessage -API "SetTenantShortName" -tenant $($Tenant.defaultDomainName) -headers $Request.Headers -message "Set Tenant ShortName '$($Request.body.ShortName)' for customer $($Tenant.defaultDomainName)" -Sev 'Info'
+        $body = [pscustomobject]@{'Results' = "Success. We've added ShortName to $($Tenant.defaultDomainName)." }
+    }
+    catch {
+        Write-LogMessage -API "SetTenantShortName" -tenant $($Tenant.defaultDomainName) -headers $Request.Headers -message "Failed to set Tenant ShortName '$($Request.body.ShortName)' for customer $($Tenant.defaultDomainName)" -Sev 'Error'
+        $body = [pscustomobject]@{'Results' = "Failed. $($_.Exception.Message)" }
+    }
 
-    $body = [pscustomobject]@{'Results' = "Success. We've Tested Invoke-ExecSetTenantShortName." }
-
-    # $TenantsTable = Get-CippTable -tablename Tenants
-
-    # if ($Request.Query.List) {
-    #     $ExcludedFilter = "PartitionKey eq 'Tenants' and Excluded eq true"
-    #     $ExcludedTenants = Get-CIPPAzDataTableEntity @TenantsTable -Filter $ExcludedFilter
-    #     Write-LogMessage -API $APINAME -headers $Request.Headers -message 'got excluded tenants list' -Sev 'Debug'
-    #     $body = @($ExcludedTenants)
-    # } elseif ($Request.query.ListAll) {
-    #     $ExcludedTenants = Get-CIPPAzDataTableEntity @TenantsTable -filter "PartitionKey eq 'Tenants'" | Sort-Object -Property displayName
-    #     Write-LogMessage -API $APINAME -headers $Request.Headers -message 'got excluded tenants list' -Sev 'Debug'
-    #     $body = @($ExcludedTenants)
-    # }
-    # try {
-    #     # Interact with query parameters or the body of the request.
-    #     $name = $Request.Query.TenantFilter
-    #     if ($Request.Query.AddExclusion) {
-    #         $Tenants = Get-Tenants -IncludeAll | Where-Object { $Request.body.value -contains $_.customerId }
-
-    #         $Excluded = foreach ($Tenant in $Tenants) {
-    #             $Tenant.Excluded = $true
-    #             $Tenant.ExcludeUser = $username
-    #             $Tenant.ExcludeDate = $date
-    #             $Tenant
-    #         }
-    #         Update-AzDataTableEntity -Force @TenantsTable -Entity ([pscustomobject]$Excluded)
-    #         Write-LogMessage -API $APINAME -tenant $($name) -headers $Request.Headers -message "Added exclusion for customer(s): $($Excluded.defaultDomainName -join ',')" -Sev 'Info'
-    #         $body = [pscustomobject]@{'Results' = "Success. Added exclusions for customer(s): $($Excluded.defaultDomainName -join ',')" }
-    #     }
-
-    #     if ($Request.Query.RemoveExclusion) {
-    #         $Tenants = Get-Tenants -IncludeAll | Where-Object { $Request.body.value -contains $_.customerId }
-    #         foreach ($Tenant in $Tenants) {
-    #             $Tenant.Excluded = $false
-    #             $Tenant.ExcludeUser = ''
-    #             $Tenant.ExcludeDate = ''
-    #             Update-AzDataTableEntity -Force @TenantsTable -Entity $Tenant
-    #         }
-    #         Write-LogMessage -API $APINAME -tenant $($name) -headers $Request.Headers -message "Removed exclusion for customer $($name)" -Sev 'Info'
-    #         $body = [pscustomobject]@{'Results' = "Success. We've removed $name from the excluded tenants." }
-    #     }
-    # } catch {
-    #     Write-LogMessage -API $APINAME -tenant $($name) -headers $Request.Headers -message "Exclusion API failed. $($_.Exception.Message)" -Sev 'Error'
-    #     $body = [pscustomobject]@{'Results' = "Failed. $($_.Exception.Message)" }
-    # }
-    # if (!$body) { $body = @() }
+    if (!$body) { $body = @() }
 
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
