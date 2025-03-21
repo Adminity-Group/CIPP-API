214769262fdba8251851ef869c57c4d552751898
ngms-psh <psh@ngms.dk>
2025-03-21 20:09:39 +0100

    Added validation of shortname

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Settings/Invoke-ExecSetTenantShortName.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Settings/Invoke-ExecSetTenantShortName.ps1
@@ -12,6 +12,17 @@ Function Invoke-ExecSetTenantShortName {
 
     Write-LogMessage -headers $Request.Headers -API $APINAME -message 'Accessed this API' -Sev 'Debug'
 
+    $regex = '^(?![0-9]+$)(?!.*\s)[a-zA-Z0-9-]{1,8}$'
+
+    if ($Request.body.ShortName -notmatch $regex) {
+        Write-LogMessage -API "SetTenantShortName" -tenant $($Tenant.defaultDomainName) -headers $Request.Headers -message "Failed to set Tenant ShortName '$($Request.body.ShortName)' for customer $($Tenant.defaultDomainName). Error: ShortName must be 6 characters or less, and can contain letters (a-z, A-Z), numbers (0-9), and hyphens. Names must not contain only numbers. Names cannot include a blank space" -Sev 'Error'
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::BadRequest
+            Body       = 'Validation Failed: ShortName must be 6 characters or less, and can contain letters (a-z, A-Z), numbers (0-9), and hyphens. Names must not contain only numbers. Names cannot include a blank space'
+        })
+        return
+    }
+
     try {
         $TenantsTable = Get-CippTable -tablename Tenants
         $Tenant = Get-Tenants -TenantFilter $Request.body.value
@@ -20,18 +31,20 @@ Function Invoke-ExecSetTenantShortName {
         Update-AzDataTableEntity -Force @TenantsTable -Entity $Tenant
         Write-LogMessage -API "SetTenantShortName" -tenant $($Tenant.defaultDomainName) -headers $Request.Headers -message "Set Tenant ShortName '$($Request.body.ShortName)' for customer $($Tenant.defaultDomainName)" -Sev 'Info'
         $body = [pscustomobject]@{'Results' = "Success. We've added ShortName to $($Tenant.defaultDomainName)." }
+
+        # Associate values to output bindings by calling 'Push-OutputBinding'.
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Body       = $body
+        })
     }
     catch {
         Write-LogMessage -API "SetTenantShortName" -tenant $($Tenant.defaultDomainName) -headers $Request.Headers -message "Failed to set Tenant ShortName '$($Request.body.ShortName)' for customer $($Tenant.defaultDomainName)" -Sev 'Error'
         $body = [pscustomobject]@{'Results' = "Failed. $($_.Exception.Message)" }
-    }
-
-    if (!$body) { $body = @() }
-
-    # Associate values to output bindings by calling 'Push-OutputBinding'.
-    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
+        # Associate values to output bindings by calling 'Push-OutputBinding'.
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::BadRequest
             Body       = $body
         })
-
+    }
 }
