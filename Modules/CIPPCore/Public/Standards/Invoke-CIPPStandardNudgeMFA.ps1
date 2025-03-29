2926cbe601e3fe652288e939ff860be96150551a
ngms-psh <psh@ngms.dk>
2025-03-29 15:34:13 +0100

    Corrected to use correct tenant id

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardNudgeMFA.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardNudgeMFA.ps1
@@ -41,12 +41,14 @@ function Invoke-CIPPStandardNudgeMFA {
         Write-Host "NudgeMFA: We're supposed to exclude a custom group. The group is $($Settings.excludeGroup)"
         try {
             $GroupNames = $Settings.excludeGroup.Split(',').Trim()
-            $TenantGroups = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/groups?$select=id,displayName&$top=999' -tenantid $TenantFilter
+            $TenantGroups = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/groups?$select=id,displayName&$top=999' -tenantid $Tenant
             Write-Host "NudgeMFA: TenantGroups: $($TenantGroups | ConvertTo-Json -Depth 5)"
-            $GroupIds = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/groups?$select=id,displayName&$top=999' -tenantid $TenantFilter |
+            $GroupIds = $TenantGroups |
                 ForEach-Object {
                     foreach ($SingleName in $GroupNames) {
+                        write-host "$($SingleName)"
                         if ($_.displayName -like $SingleName) {
+                            write-host "$($_.id)"
                             $_.id
                         }
                     }
@@ -138,3 +140,149 @@ function Invoke-CIPPStandardNudgeMFA {
         Add-CIPPBPAField -FieldName 'NudgeMFA' -FieldValue $StateIsCorrect -StoreAs bool -Tenant $Tenant
     }
 }
+
+
+
+$groups = @"
+[
+  {
+    "id": "05d0e47f-1f10-47c0-83cd-8c47b775170d",
+    "displayName": "Ng.PIM.Cust.Az.Sub.Contributor"
+  },
+  {
+    "id": "071abcea-19f3-438e-b7fc-020dd37e2ccc",
+    "displayName": "Azure ATP ngmsdk Users"
+  },
+  {
+    "id": "1c97b7e7-0d96-4d0c-87f2-9a0588d3383e",
+    "displayName": "M365 GDAP Cloud Application Administrator"
+  },
+  {
+    "id": "1e2a903a-598e-4940-8cdc-542fbcda5583",
+    "displayName": "All Company"
+  },
+  {
+    "id": "210e9a6c-2e87-43bf-b3eb-82b2442d1aa2",
+    "displayName": "M365 GDAP Exchange Administrator"
+  },
+  {
+    "id": "28cf90e0-841f-4544-8376-8cd31277402b",
+    "displayName": "PartnerCenter_MPNAdmin"
+  },
+  {
+    "id": "2c16e5bf-f387-45fd-b32e-96ba92ebdd8c",
+    "displayName": "M365 GDAP Application Administrator"
+  },
+  {
+    "id": "2db46348-cf95-400e-8a57-e2ebd9881b21",
+    "displayName": "NgMS Consult"
+  },
+  {
+    "id": "2dba24d6-8526-485f-b109-973a18d2d0b7",
+    "displayName": "Ng: SG_Intune.AS.Test Users"
+  },
+  {
+    "id": "2fbb07cb-ea8c-4754-970a-f26e02afc331",
+    "displayName": "M365 GDAP Cloud Device Administrator"
+  },
+  {
+    "id": "45fed89b-7431-4f12-9ea1-32ba0b052669",
+    "displayName": "M365 GDAP Helpdesk Administrator"
+  },
+  {
+    "id": "49154ebf-f4c1-4a9f-9ecc-261411fc476d",
+    "displayName": "M365 GDAP Intune Administrator"
+  },
+  {
+    "id": "4ee03f26-b2ad-4cb7-af49-952fc9c39405",
+    "displayName": "SalesAgents"
+  },
+  {
+    "id": "530e6fbe-204f-41e3-8574-7a7c9a3df1a0",
+    "displayName": "M365 GDAP Privileged Authentication Administrator"
+  },
+  {
+    "id": "683f63ac-cefe-411b-bd1d-b5ddd8606ab1",
+    "displayName": "Azure ATP ngmsdk Viewers"
+  },
+  {
+    "id": "6a30a450-1a20-49fd-b5aa-312a744dd379",
+    "displayName": "Ng.PIM.Cust.Az.Sub.Reader"
+  },
+  {
+    "id": "71041b9d-8f95-49c0-944d-b165909e8bc2",
+    "displayName": "M365 GDAP SharePoint Administrator"
+  },
+  {
+    "id": "7515b87e-c8f3-4c5b-b35b-4a203960dd02",
+    "displayName": "NgSentinel"
+  },
+  {
+    "id": "84bc732b-744c-4774-a986-aaa8bc7173b6",
+    "displayName": "M365 GDAP Privileged Role Administrator"
+  },
+  {
+    "id": "858a24d3-729b-47f0-8375-2190c7444d29",
+    "displayName": "M365 GDAP Security Administrator"
+  },
+  {
+    "id": "9c6ca232-2073-4466-b369-20820743218c",
+    "displayName": "M365 GDAP License Administrator"
+  },
+  {
+    "id": "a19576ea-8c41-442d-a47a-f0b65f6f409b",
+    "displayName": "M365 GDAP User Administrator"
+  },
+  {
+    "id": "aa532010-f144-4cad-9c33-fba767e3a043",
+    "displayName": "M365 GDAP Authentication Policy Administrator"
+  },
+  {
+    "id": "ad6352f5-5bee-44b4-89d0-9859d2b52bc0",
+    "displayName": "M365 GDAP Billing Administrator"
+  },
+  {
+    "id": "b8f963c7-bfd3-464a-b758-a5bbe2b8da9c",
+    "displayName": "M365 GDAP Global Reader"
+  },
+  {
+    "id": "c8dc5c2c-ee84-46c3-80dc-436005c90cf6",
+    "displayName": "All Users"
+  },
+  {
+    "id": "d0da3416-b9ec-4978-9992-d55fdc0b16ba",
+    "displayName": "HelpdeskAgents"
+  },
+  {
+    "id": "d8128a22-a750-43e9-9b46-3f35f0152a7f",
+    "displayName": "M365 GDAP Cloud App Security Administrator"
+  },
+  {
+    "id": "d8dd5a7a-e2a5-43f1-afd9-fc7c44629e29",
+    "displayName": "AdminAgents"
+  },
+  {
+    "id": "d9d3eee6-80fe-4f5b-937f-873ed814fda5",
+    "displayName": "Azure ATP ngmsdk Administrators"
+  },
+  {
+    "id": "da9927e3-695f-40e2-b8bc-ad92081cf785",
+    "displayName": "M365 GDAP Service Support Administrator"
+  },
+  {
+    "id": "fd17768b-7a1c-43c6-88d4-398f0ca59d39",
+    "displayName": "NgMS.Sec.Ass.Acronis SSO"
+  },
+  {
+    "id": "fd77a303-fbef-4cbb-9964-c87e908f6c74",
+    "displayName": "M365 GDAP Teams Administrator"
+  }
+]
+"@ | ConvertFrom-Json
+
+
+$settings = @"
+{"Tenant":"folkelarsen.dk","Standard":"NudgeMFA","Settings":{"remediate":true,"alert":false,"report":false,"state":{"label":"Enabled","value":"enabled"},"snoozeDurationInDays":"7","excludeGroup":"Ng.L1: EntraID.Exclude.RegistrationCampaign"},"QueueId":"82c7a16f-cd61-44ad-bcdc-4e304aac603c","templateId":"4058f1ff-e499-473f-986a-90922149358f","QueueName":"NudgeMFA - folkelarsen.dk","FunctionName":"CIPPStandard"}
+"@ | ConvertFrom-Json
+
+$settings = $settings.Settings
